9267b41e485690a75c0dfe6f023569929866d2bc
Rakesh Kumar <haiimrakesh@gmail.com>
2025-02-01 16:05:21 +0530

    fixed solution file and removed Iron Barcode reference.

--- Tsade.Barcode.Test/UnitTest1.cs
+++ Tsade.Barcode.Test/UnitTest1.cs
@@ -1,4 +1,4 @@
-using Tsade.Barcode.Web;
+
 namespace Tsade.Barcode.Test;
 
 public class GenerateBarcodeTest
@@ -6,6 +6,5 @@ public class GenerateBarcodeTest
     [Fact]
     public void Generate_ShouldGenerateAnPngImage()
     {
-        Barcode.Web.Domain.IronBarCodeGenerator.Generate("Saree128","image.png");
     }
 }
